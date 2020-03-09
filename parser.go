2a8a4c55608a4b1c1a4687358179b44d5641b494
alseiitov <dossan.alseiit@gmail.com>
2020-03-09 12:39:20 +0600

    deleted repeating functions

--- parser.go
+++ parser.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
 //Main struct
 var All API
 
@@ -9,8 +17,23 @@ func parseJSON() {
 	urlLocations := "https://groupietrackers.herokuapp.com/api/locations"
 	urlDates := "https://groupietrackers.herokuapp.com/api/dates"
 	urlRelation := "https://groupietrackers.herokuapp.com/api/relation"
-	All.Artists, _ = ParseArtists(urlArtists)
-	All.Locations, _ = ParseLocations(urlLocations)
-	All.Dates, _ = ParseDates(urlDates)
-	All.Relation, _ = ParseRelation(urlRelation)
+
+	ParseInfo(urlArtists, &All.Artists)
+	ParseInfo(urlLocations, &All.Locations)
+	ParseInfo(urlDates, &All.Dates)
+	ParseInfo(urlRelation, &All.Relation)
+}
+
+func ParseInfo(url string, temp interface{}) {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+	json.Unmarshal(body, &temp)
 }
